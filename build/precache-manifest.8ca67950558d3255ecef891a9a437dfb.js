37b4db7575fbe08141146ffcd4ccbc1a5dd8bcd5
connerstakein <54186477+connerstakein@users.noreply.github.com>
2021-11-12 08:01:49 -0800

    Add files via upload

---
+++ build/precache-manifest.8ca67950558d3255ecef891a9a437dfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b67b19ccf36e6220038a53c1c93d968",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db4fdbab83c75974be53",
+    "url": "./static/css/2.1877ba53.chunk.css"
+  },
+  {
+    "revision": "7a2165f4834eb4be8a80",
+    "url": "./static/css/main.36b53d24.chunk.css"
+  },
+  {
+    "revision": "db4fdbab83c75974be53",
+    "url": "./static/js/2.2df47789.chunk.js"
+  },
+  {
+    "revision": "7f82bbd88f6d9d18c29316f02f37eaf0",
+    "url": "./static/js/2.2df47789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a2165f4834eb4be8a80",
+    "url": "./static/js/main.645c56dc.chunk.js"
+  },
+  {
+    "revision": "d852897cea0a694b3726",
+    "url": "./static/js/runtime-main.c06bb91b.js"
+  }
+]);
